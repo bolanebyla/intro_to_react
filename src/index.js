2943a007c67d3b5a07e7b7bd7947256f4f437db6
Алексей <alexleskov2000@gmail.com>
2021-04-26 11:09:11 +0800

    Вывод списка ходов

--- src/index.js
+++ src/index.js
@@ -79,6 +79,18 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Перейти к ходу #' + move :
+                'К началу игры';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) {
             status = 'Выиграл ' + winner;
@@ -96,7 +108,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
